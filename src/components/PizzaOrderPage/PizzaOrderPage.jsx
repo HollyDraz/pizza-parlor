d1ce9ea81502783d2a6e85a9538edce4a0702b8a
Holly Drazenovich <hollydraze14@gmail.com>
2022-09-21 11:41:16 -0500

    added some basic setup for pizza order page

--- src/components/PizzaOrderPage/PizzaOrderPage.jsx
+++ src/components/PizzaOrderPage/PizzaOrderPage.jsx
@@ -11,4 +11,24 @@
     */
 
 // select pizza 
+//imports
+import {useSelector, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom';
 
+const PizzaSelect = () => {
+    //history to next page 
+    const history = useHistory();
+    //create setters and getters 
+    const dispatch = useDispatch();
+    //create getter here 
+
+    return(
+        <>
+        <div>
+            <h3>Select pizza</h3>
+            
+            <button>next page</button>
+        </div>
+        </>
+    )
+}
