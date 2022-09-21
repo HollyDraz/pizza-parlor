797036487af86d0cab7da9a76f960d6a86efec37
Holly Drazenovich <hollydraze14@gmail.com>
2022-09-21 11:37:03 -0500

    added a few code commetns for holly to-do page

--- src/components/PizzaOrderPage/PizzaOrderPage.jsx
+++ src/components/PizzaOrderPage/PizzaOrderPage.jsx
@@ -1,12 +1,14 @@
 // Pizza order page: Holly 
 // To Do:
 /*# PIZZA ORDER PAGE
-- [ ] Header component
 - [ ] Total: with shopping cart
 - [ ] Redux for Total
 - [ ] Body
-- [ ] Append our 'GET' of Pizza options.
-- [ ] Axios request
+- [ ] Append our 'GET' of Pizza options.//hold 
+- [ ] Axios request // hold 
 - [ ] Next Navigation (Router/Link) to 
     Order Detail page
     */
+
+// select pizza 
+
