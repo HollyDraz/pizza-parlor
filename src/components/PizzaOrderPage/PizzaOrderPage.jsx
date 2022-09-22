699a765be24aa4429027dd23a4bd8e070bc8fa46
Holly Drazenovich <hollydraze14@gmail.com>
2022-09-22 11:00:49 -0500

    added pizza list to pizza order page

--- src/components/PizzaOrderPage/PizzaOrderPage.jsx
+++ src/components/PizzaOrderPage/PizzaOrderPage.jsx
@@ -1,19 +1,42 @@
 import {useSelector, useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const PizzaSelect = () => {
     //history to next page 
-    const history = useHistory();
-    //create setters and getters 
+    const history = useHistory(); 
     const dispatch = useDispatch();
-    //create getter here 
+    let [pizzaList, setPizzaList] = useState([]);
+
+    useEffect(() => {
+        console.log('useEffect - page load');
+        getPizza();
+      }, []); 
+
+    const getPizza = () => {
+        axios({
+            method: 'GET',
+            url: '/api/pizza'
+        }).then((response) => {
+           setPizzaList(response.data);
+        }).catch((err)=>{
+            console.log(err, 'problem in GET for pizza order page');
+        });
+    };
+    
 
     return(
         <>
         <div>
             <h3>Select pizza</h3>
-            
-            <button>next page</button>
+            <ul>
+                {pizzaList.map((pizza) =>
+                <li key={pizza.id}>{pizza.name} {pizza.description} {pizza.price} </li> 
+                )}
+                
+            </ul>
+            <button onClick={() => history.push('/order_details')}> Next page</button>
         </div>
         </>
     )
