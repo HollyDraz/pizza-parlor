febcdd5754331e65c580795100853d1b9ab6a998
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-09-24 08:57:17 -0500

    updated handlePizza in PizzaOrderPage to add a quantity property with a value of 1 to the pizzaInput object before dipatching to order reducer, also included dispatch to totalCost reducer to update the cost with each menu item click

--- src/components/PizzaOrderPage/PizzaOrderPage.jsx
+++ src/components/PizzaOrderPage/PizzaOrderPage.jsx
@@ -3,11 +3,13 @@ import { useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+
 const PizzaSelect = () => {
     //history to next page 
     const history = useHistory(); 
     const dispatch = useDispatch();
     let [pizzaList, setPizzaList] = useState([]);
+    const order = useSelector(store => store.order);
 
     useEffect(() => {
         console.log('useEffect - page load');
@@ -27,8 +29,10 @@ const PizzaSelect = () => {
     
     const handlePizza = (pizzaInput) => {
         console.log('in handle pizza', pizzaInput);
+        console.log(order.includes(pizzaInput.id));
+        pizzaInput.quantity = 1;
         dispatch({type: 'SET_ORDER', payload: pizzaInput});
-        
+        dispatch({type: 'SET_TOTAL_COST', payload: Number(pizzaInput.price)});
     }
 
     return(
